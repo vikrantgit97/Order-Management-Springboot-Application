735569c98d16c04a475ef2d86431fd04fa3bbe4c
Vikrant <vikrantgopewar@gmail.com>
2023-06-17 09:38:08 +0530

    added readme.md file

--- src/main/java/com/ordermanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ordermanagement/exception/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.ordermanagement.exception;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.ordermanagement.dto.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,35 +9,36 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import com.ordermanagement.dto.ErrorDetails;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	// handle specific exceptions
-	@ExceptionHandler(CustomerException.class)
-	public ResponseEntity<ErrorDetails> CustomerEcxeptionHandle(CustomerException ce, WebRequest webRequest) {
-		ErrorDetails error = new ErrorDetails(new Date(), ce.getMessage(), webRequest.getDescription(false));
-		return new ResponseEntity<ErrorDetails>(error, HttpStatus.NOT_FOUND);
-	}
+    // handle specific exceptions
+    @ExceptionHandler(CustomerException.class)
+    public ResponseEntity<ErrorDetails> CustomerEcxeptionHandler(CustomerException customerException, WebRequest webRequest) {
+        ErrorDetails error = new ErrorDetails(new Date(), customerException.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.NOT_FOUND);
+    }
 
-	// global exceptions
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorDetails> ExceptionHandler(Exception exception, WebRequest webRequest) {
-		ErrorDetails error = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
-		return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    // global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> ExceptionHandler(Exception exception, WebRequest webRequest) {
+        ErrorDetails error = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Object> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException manve,
-			WebRequest webRequest) {
-		Map<String, String> errors = new HashMap<>();
-		manve.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String message = error.getDefaultMessage();
-			errors.put(fieldName, message);
-		});
-
-		return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
-	}
+    // method argument validation exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException methodArgumentNotValidException, WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+        methodArgumentNotValidException.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
